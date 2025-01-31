33fcf0c68b40d598381e7fe9f41396865f4e1c69
João Víctor Guedes <joaoguedes1404@gmail.com>
2025-01-31 17:08:02 -0300

    Adicionando margem

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -4,7 +4,7 @@ import airplaneLogo from "../../assets/logo.svg";
 
 export default function NavBar() {
   return (
-    <div className="w-full h-44 md:h-24 bg-gray-50">
+    <div className="w-full h-44 md:h-24 bg-gray-50 m-4">
       <div className="container max-w-5xl mx-auto p-4 h-44 md:h-24 flex flex-col md:flex-row items-center bg-gray-50">
         <div className="font-black flex">
           <img src={airplaneLogo} alt="logo" />
