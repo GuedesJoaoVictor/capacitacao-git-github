62bf7908fbac3b26036350c47e0259b771a92c40
João Víctor Guedes <joaoguedes1404@gmail.com>
2025-01-31 18:15:33 -0300

    Adicionando responsividade

--- src/components/AboutUs/AboutUs.tsx
+++ src/components/AboutUs/AboutUs.tsx
@@ -3,10 +3,10 @@ import airplaneImage from "../../assets/avião-da-viagem-aérea-31660569.webp";
 export default function AboutUs() {
   return (
     <div className="w-full">
-      <div className="container md:max-w-4xl mx-auto flex space-x-8 items-center justify-center">
-        <div className="m-4">
+      <div className="container md:max-w-4xl mx-auto flex md:flex-row flex-col space-x-8 items-center justify-center">
+        <div className="m-4 text-center">
           <div className="text-2xl text-blue-600 font-mono">QUEM SOMOS</div>
-          <p>
+          <p className="m-4 text-justify">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere ut
             ratione consectetur. Fugit voluptatem, aperiam velit quisquam
             architecto tempore nisi accusantium reiciendis delectus laboriosam
