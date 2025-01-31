991db119d234ac93254d453185ef82ff1cb759fc
João Víctor Guedes <joaoguedes1404@gmail.com>
2025-01-31 17:08:27 -0300

    Adicionando imagem avião

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import NavBar from "./components/navbar/NavBar";
+import airPlane from "./assets/viagem-programada-1-1.jpg";
 
 export default function App() {
   return (
     <div className="w-screen h-screen">
       <NavBar />
+      <div className="w-full">
+        <img
+          src={airPlane}
+          alt="airplane"
+          className="w-full object-cover md:h-[30vw] h-[50vw]"
+        />
+      </div>
     </div>
   );
 }
