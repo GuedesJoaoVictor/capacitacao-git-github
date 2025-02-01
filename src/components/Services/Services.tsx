0590564fba6156e5c8dc2d112a8c47168cb19587
João Víctor Guedes <joaoguedes1404@gmail.com>
2025-02-01 13:35:39 -0300

    Adicionando espaçamento e border radius

--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -8,11 +8,11 @@ import airplaneImage from "../../assets/airplane_3.jpg";
 export default function Services() {
   return (
     <div className="w-full bg-gray-50">
-      <div className="container max-w-5xl mx-auto py-2">
+      <div className="container max-w-5xl mx-auto py-8">
         <div className="text-center w-full text-sky-700 text-3xl my-8">
           SERVIÇOS
         </div>
-        <div className="grid grid-cols-3 grid-rows-2">
+        <div className="grid grid-cols-3 grid-rows-2 rounded overflow-hidden">
           <ServicesGridItem title="RESERVA DE HOTÉIS" image={romaImage} />
           <ServicesGridItem title="PACOTES DE VIAGEM" image={shipImage} />
           <ServicesGridItem // Imagem com espaçamento 2
